ef4e3b82b352f272b2d002cca86d4b0a46f52de9
Alexksandr <archangel7017@gmail.com>
2017-07-31 15:59:25 +0300

    datetime

---
+++ src/main/java/romaniancoder/booking/ViewController.java
@@ -0,0 +1,24 @@
+package romaniancoder.booking;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+
+/**
+ * Created by Александр on 29.07.2017.
+ */
+
+@Controller
+public class ViewController {
+
+    @RequestMapping("/")
+    public String index(Model model){
+
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("username", "Romanian Coder");
+
+        return "index";
+    }
+}
