1632dd9c648a6af95d10c3b550c15bb05c82f4e5
Alexksandr <archangel7017@gmail.com>
2017-08-01 10:31:36 +0300

    configuration

--- src/main/java/romaniancoder/booking/ViewController.java
+++ src/main/java/romaniancoder/booking/ViewController.java
@@ -1,5 +1,7 @@
 package romaniancoder.booking;
 
+import org.omg.CORBA.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,9 @@ import java.util.Date;
 
 @Controller
 public class ViewController {
+
+    @Value("${app-mode}")
+    private String appMode;
 
     @RequestMapping("/")
     public String index(Model model){
